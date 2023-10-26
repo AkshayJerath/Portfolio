ac532d3f07d7d5e791012de0dea719e0e3f86b09
MrFYA <113229852+MrFYA@users.noreply.github.com>
2023-10-27 03:11:55 +0530

    Update Info.js

--- src/info/Info.js
+++ src/info/Info.js
@@ -2,32 +2,18 @@ import self from "../img/self.png"
 import mock1 from "../img/mock1.png"
 import mock2 from "../img/mock2.png"
 import { FaFileDownload } from "react-icons/fa";
-/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
-how to make this template work best for you! */
 
 export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
-/*
-I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
-These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
-the site.
- */
-
-
-/*
-So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
-that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
-of what each of the values mean.
- */
 
 export const info = {
     firstName: "Akshay",
     lastName: "Jerath",
-    initials: "AJ", // the example uses first and last, but feel free to use three or more if you like.
+    initials: "AJ", 
     position: "a want to be a Cybersecurity Analyst",
-    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
-    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+    selfPortrait: self,
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`, 
     baseColor: colors[0],
-    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+    miniBio: [
         {
             emoji: '☕',
             text: 'Fueled by Tea'
@@ -76,8 +62,6 @@ export const info = {
             icon: FaFileDownload,
             label: 'Resume'
         }
-// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
-// Just change the links so that they lead to your social profiles.
 
     ],
     bio: "Hello! I'm Akshay. I'm a student at Vellore Institute Of Technology(Bhopal), currently learning B.Tech.Computer Science and Engineering with (Specialization in Cyber security and Digital Forensics).Fully committed to my degree trying to learn from various experience I have had my time in the college. Trying to make the best of what Ican learn in college years.You should hire me!",
@@ -104,13 +88,11 @@ export const info = {
             label: 'Cooking',
             emoji: '🌶'
         },
-// Same as above, change the emojis to match / relate to your hobbies or interests.
-// You can also remove the emojis if you'd like, I just think they look cute :P
-    ],
-    portfolio: [ // This is where your portfolio projects will be detailed
+
+    portfolio: [ 
         {
             title: "2 Factor Authentication",
-            source: "https://github.com/MrFYA/2-Factor-Authentication", // this should be a link to the **repository** of the project, where the code is hosted.
+            source: "https://github.com/MrFYA/2-Factor-Authentication",
             image: mock1
         },
         {
